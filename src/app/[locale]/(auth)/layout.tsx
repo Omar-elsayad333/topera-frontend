d1fabb74219983cc4ac42c85d594b065c12377b3
Mohamed Ahmed Ragheb <MohamedRagheb@geexar.dev>
2024-03-18 01:16:16 +0200

    change tempelete to layout

---
+++ src/app/[locale]/(auth)/layout.tsx
@@ -0,0 +1,48 @@
+import { Box, Grid } from '@mui/material'
+import Image from 'next/image'
+import logo from '@/assets/images/logo.svg'
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <Grid
+      xs={12}
+      lg={12}
+      md={12}
+      container
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+        backgroundImage: `url(https://s3-alpha-sig.figma.com/img/f19d/933c/6f2efafd34ea15e733cacfcf5b43be87?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=FQcQDLVqwP9POSCs7u2-nYJyqYpFnDjSGWFuN~Hw4KNNkxcuOekUvr~1GhmANQ-wusW3SvjU4pxA2halsTA0qj0wfkr4IwLc0r8c~vouJYY5ZPcsHSr8QeWZwkJFpHUl0La8rnIQSyJZpWTg3OBlzWN4WvW4WgfmDqb-pgAxoae8cGJXroFdZbrFkp~l0viEWJ~XQSzaR5SOvrll6P7xyQHpjPCZdvO8sIayFGjINzwYeIA5seY~NSmQ5F-mlC9W9RB4vvgZSvu0QCgLtliOxVSeVnEdmfNhu7daQVMEU8YR7SmjDRJcKM~IIKvNDIfQuR6S0mkxtWYN0~sG8e9NSw) !important`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <Grid
+        xs={11}
+        lg={4}
+        md={4}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          backgroundColor: 'white',
+          border: '1px solid transparent',
+          borderRadius: '8px',
+          padding: '34px 75px',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Image height={48} width={51} src={logo} alt={'topera logo'} />
+        </Box>
+        <Grid xs={12}>{children}</Grid>
+      </Grid>
+    </Grid>
+  )
+}
--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -1,42 +1,24 @@
 import React from 'react'
-import { Box } from '@mui/material'
-import Image from 'next/image'
-import logo from '@/assets/images/logo.svg'
+import Typography from '@mui/material/Typography'
+import { Grid } from '@mui/material'
+import Link from 'next/link'
+
 const logIn: React.FC = () => {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: '100vh',
-        backgroundImage: `url(https://s3-alpha-sig.figma.com/img/f19d/933c/6f2efafd34ea15e733cacfcf5b43be87?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=FQcQDLVqwP9POSCs7u2-nYJyqYpFnDjSGWFuN~Hw4KNNkxcuOekUvr~1GhmANQ-wusW3SvjU4pxA2halsTA0qj0wfkr4IwLc0r8c~vouJYY5ZPcsHSr8QeWZwkJFpHUl0La8rnIQSyJZpWTg3OBlzWN4WvW4WgfmDqb-pgAxoae8cGJXroFdZbrFkp~l0viEWJ~XQSzaR5SOvrll6P7xyQHpjPCZdvO8sIayFGjINzwYeIA5seY~NSmQ5F-mlC9W9RB4vvgZSvu0QCgLtliOxVSeVnEdmfNhu7daQVMEU8YR7SmjDRJcKM~IIKvNDIfQuR6S0mkxtWYN0~sG8e9NSw) !important`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
-    >
-      <Box
-        sx={{
-          minWidth: '699px',
-          display: 'flex',
-          flexDirection: 'column',
-          backgroundColor: 'white',
-          border: '1px solid transparent',
-          borderRadius: '8px',
-        }}
-      >
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <Image height={48} width={51} src={logo} alt={'topera logo'} />
-        </Box>
-      </Box>
-    </Box>
+    <Grid container columns={12}>
+      <Grid container item columns={12}>
+        <Typography sx={{ fontWeight: '500' }} variant={'h3'}>
+          Sign in
+        </Typography>
+        <Grid item container columns={12}>
+          <Typography>New user?</Typography>
+          <Link href={'/signup'}>
+            <Typography sx={{ color: '#1473E6', marginLeft: '5px' }}>Create a new account</Typography>
+          </Link>
+        </Grid>
+      </Grid>
+      <Grid item container></Grid>
+    </Grid>
   )
 }
 
