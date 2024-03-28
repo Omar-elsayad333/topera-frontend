53ec4e667d5a0d7d70314913b18a803285d883a9
MohamedRagheb <98286948+MohamedRagheb@users.noreply.github.com>
2024-03-28 16:08:26 +0200

    delete grid tag

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -8,58 +8,50 @@ interface IPropsWithChildren {
 
 export default function Layout({ children }: IPropsWithChildren) {
   return (
-    <main>
+    <main
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100dvh',
+        backgroundImage: `url('${authBg.src}')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
       <Grid
         rowGap={'25px'}
-        xs={12}
-        lg={12}
-        md={12}
-        container
         item
+        container
+        xs={10}
+        lg={4}
+        md={7}
         sx={{
+          maxWidth: '700px',
           display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          width: '100%',
-          height: '100dvh',
-          backgroundImage: `url('${authBg.src}')`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
+          flexDirection: 'column',
+          backgroundColor: '#F4F4FF',
+          border: '1px solid transparent',
+          borderRadius: '8px',
+          padding: {
+            lg: '34px 75px',
+            md: '34px 75px',
+            xs: '34px 55px',
+          },
         }}
       >
-        <Grid
-          rowGap={'25px'}
-          item
-          container
-          xs={10}
-          lg={4}
-          md={7}
+        <Box
           sx={{
-            maxWidth: '700px',
             display: 'flex',
-            flexDirection: 'column',
-            backgroundColor: '#F4F4FF',
-            border: '1px solid transparent',
-            borderRadius: '8px',
-            padding: {
-              lg: '34px 75px',
-              md: '34px 75px',
-              xs: '34px 55px',
-            },
+            justifyContent: 'center',
+            alignItems: 'center',
           }}
         >
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <Image height={48} width={51} src={logo} alt={'topera logo'} />
-          </Box>
-          <Grid item rowGap={'20px'} container xs={12}>
-            {children}
-          </Grid>
+          <Image height={48} width={51} src={logo} alt={'topera logo'} />
+        </Box>
+        <Grid item rowGap={'20px'} container xs={12}>
+          {children}
         </Grid>
       </Grid>
     </main>
