9f8dbcc3a0f4acd6d75c51e51090849e9553b27f
MohamedRagheb <modyahmed221@gmail.com>
2024-04-26 15:59:32 +0300

    fix responsive

--- src/app/[locale]/(main)/profile/page.tsx
+++ src/app/[locale]/(main)/profile/page.tsx
@@ -10,12 +10,12 @@ import { NextPage } from 'next'
 
 const Profile: NextPage = () => {
   return (
-    <Grid container direction={'row-reverse'} columnGap={'30px'} margin={'60px 0'}>
-      <Grid container item gap={'40px'} lg={3} md={12} xs={12}>
+    <Grid container direction={'row-reverse'} columnGap={'30px'} rowGap={'30px'} margin={'60px 0'}>
+      <Grid container item gap={'40px'} lg={3} md={3} xs={12}>
         <Intro />
         <Resume />
       </Grid>
-      <Grid lg={8} container item sx={{ backgroundColor: 'blue' }}>
+      <Grid lg={8} md={8} container item sx={{ backgroundColor: 'blue' }}>
         hero sections
       </Grid>
     </Grid>
