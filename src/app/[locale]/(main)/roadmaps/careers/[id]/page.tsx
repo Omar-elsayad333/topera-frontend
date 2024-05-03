5b332ad554ac3b99d9540c8fae071a56220a3edd
Omar-elsayad333 <omarelsayad313@gmail.com>
2024-05-03 16:49:01 +0300

    finishing careers page

--- src/app/[locale]/(main)/roadmaps/careers/[id]/page.tsx
+++ src/app/[locale]/(main)/roadmaps/careers/[id]/page.tsx
@@ -1,25 +1,19 @@
-import Image from 'next/image'
 import { Metadata } from 'next'
-import { CSSProperties } from 'react'
 import { Orbitron } from 'next/font/google'
 
 // Server Actions
 import { serverAction } from '@/services/actions'
-
-// Assets
-import RightArrow from '@/assets/icons/rightArrow.svg'
-import GraduateIcon from '@/assets/icons/graduateIcon.svg'
 
 // Types
 import { ICareer } from '@/types/pages/roadmaps'
 
 // Components
 import TrackTabComponent from '@/components/pages/Roadmaps/TrackTabComponent'
+import CareerButtonComponent from '@/components/pages/Roadmaps/CareerButtonComponent'
 
 // MUI
 import Grid from '@mui/material/Grid'
 import Stack from '@mui/material/Stack'
-import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 
 // Google Font
@@ -40,31 +34,12 @@ export const metadata: Metadata = {
 
 const RoadmapsCareer = async ({ params }: IProps) => {
   const { data }: { data: ICareer } = await serverAction(`/roadmaps/${params.id}`)
-  const styles: Record<string, CSSProperties> = {
-    buttonStyle: {
-      padding: '14px 22px',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      gap: '20px',
-      width: '550px',
-      maxWidth: '100%',
-      borderRadius: '10px',
-      border: `1px solid #00000066`,
-    },
-  }
 
   return (
     <Grid container sx={{ py: '64px' }} spacing={4}>
       <Grid item xs={12} lg={7}>
         <Stack gap={'24px'} sx={{ mb: '56px' }}>
-          <Button sx={styles.buttonStyle}>
-            <Stack direction={'row'} gap={2} alignItems={'center'} justifyContent={'flex-start'}>
-              <Image width={30} height={30} alt={data.name} src={GraduateIcon} />
-              <Typography color={'black'} variant="h6">{`What is ${data.name}?`}</Typography>
-            </Stack>
-            <Image alt={data.name} src={RightArrow} />
-          </Button>
+          <CareerButtonComponent articalId={data.article.id} careerName={data.name} />
           <Typography fontWeight={500} color={'primary'} variant="h5">
             <p className={`${orbitron.className}`}>Defintion</p>
           </Typography>
---
+++ src/components/pages/Roadmaps/CareerButtonComponent/index.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import Image from 'next/image'
+import { CSSProperties, useState } from 'react'
+
+// Assets
+import RightArrow from '@/assets/icons/rightArrow.svg'
+import GraduateIcon from '@/assets/icons/graduateIcon.svg'
+
+// Components
+import ArticleComponent from '@/components/pages/Roadmaps/ArticleComponent'
+
+// MUI
+import Stack from '@mui/material/Stack'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+
+const CareerButtonComponent = ({ careerName, articalId }: { careerName: string; articalId: string }) => {
+  const [drawerState, setDrawerState] = useState(false)
+
+  const styles: Record<string, CSSProperties> = {
+    buttonStyle: {
+      padding: '14px 22px',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: '20px',
+      width: '550px',
+      maxWidth: '100%',
+      borderRadius: '10px',
+      border: `1px solid #00000066`,
+    },
+  }
+
+  const handleDrawer = (state: boolean) => {
+    setDrawerState(state)
+  }
+
+  return (
+    <>
+      <Button onClick={() => handleDrawer(true)} sx={styles.buttonStyle}>
+        <Stack direction={'row'} gap={2} alignItems={'center'} justifyContent={'flex-start'}>
+          <Image width={30} height={30} alt={careerName} src={GraduateIcon} />
+          <Typography color={'black'} variant="h6">{`What is ${careerName}?`}</Typography>
+        </Stack>
+        <Image alt={careerName} src={RightArrow} />
+      </Button>
+      <ArticleComponent id={articalId} handleDrawer={handleDrawer} open={drawerState} />
+    </>
+  )
+}
+
+export default CareerButtonComponent
