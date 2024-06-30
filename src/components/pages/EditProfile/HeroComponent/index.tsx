4991c04eab40db89a7281965112067ad1c70b41a
MohamedRagheb <98286948+MohamedRagheb@users.noreply.github.com>
2024-07-01 00:56:22 +0300

    handle back to profile

--- src/components/pages/EditProfile/HeroComponent/index.tsx
+++ src/components/pages/EditProfile/HeroComponent/index.tsx
@@ -2,6 +2,8 @@
 
 // Mui
 import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
 
 // Components
 import DescriptionComponent from '@/components/pages/EditProfile/DescriptionComponent'
@@ -9,42 +11,58 @@ import Social from '@/components/pages/EditProfile/Social'
 import WorkExperience from '@/components/pages/EditProfile/WorkExperience'
 import Education from '@/components/pages/EditProfile/Education'
 import Skills from '@/components/pages/EditProfile/Skills'
+import BasicInformation from '@/components/pages/EditProfile/BasicInformation'
 
 // Hooks
 import useProfile from '@/components/pages/EditProfile/useProfile'
-import BasicInformation from '@/components/pages/EditProfile/BasicInformation'
+import { useTranslations } from 'next-intl'
+import { useRouter } from 'next/navigation'
 
 export default function EditProfileHeroComponent() {
+  const router = useRouter()
+
   const {
     data: { profileData },
   } = useProfile()
 
+  const tEditProfile = useTranslations('edit_profile')
   return (
-    <Grid
-      container
-      display={'flex'}
-      direction={{ md: 'row', lg: 'row' }}
-      columnGap={'30px'}
-      rowGap={'30px'}
-      margin={'60px 0'}
-    >
-      <Grid height={'fit-content'} gap={'16px'} container justifyItems={'start'} item lg={3} md={3} xs={12}></Grid>
-      <Grid lg={8} md={8} rowGap={'16px'} container item>
-        <BasicInformation
-          data={{
-            firstName: profileData?.firstName!,
-            lastName: profileData?.lastName!,
-            ...(profileData?.city ? { city: { name: profileData.city } } : null),
-            ...(profileData?.country ? { country: { name: profileData.country } } : null),
-            occupation: profileData?.occupation!,
-          }}
-        />
-        <DescriptionComponent value={profileData?.bio} />
-        <Social data={profileData?.socials} />
-        <WorkExperience experiences={profileData?.experiences} />
-        <Education educations={profileData?.educations} />
-        <Skills />
+    <div>
+      <Box sx={{ width: '100%', background: '#fff', padding: '13px 0', display: 'flex', justifyContent: 'center' }}>
+        <Button
+          sx={{ height: '46px', backgroundColor: '#4E4EBC' }}
+          variant={'contained'}
+          onClick={() => router.push('/profile')}
+        >
+          {tEditProfile('back_to_profile')}
+        </Button>
+      </Box>
+      <Grid
+        container
+        display={'flex'}
+        direction={{ md: 'row', lg: 'row' }}
+        columnGap={'30px'}
+        rowGap={'30px'}
+        margin={'60px 0'}
+      >
+        <Grid height={'fit-content'} gap={'16px'} container justifyItems={'start'} item lg={3} md={3} xs={12}></Grid>
+        <Grid lg={8} md={8} rowGap={'16px'} container item>
+          <BasicInformation
+            data={{
+              firstName: profileData?.firstName!,
+              lastName: profileData?.lastName!,
+              ...(profileData?.city ? { city: { name: profileData.city } } : null),
+              ...(profileData?.country ? { country: { name: profileData.country } } : null),
+              occupation: profileData?.occupation!,
+            }}
+          />
+          <DescriptionComponent value={profileData?.bio} />
+          <Social data={profileData?.socials} />
+          <WorkExperience experiences={profileData?.experiences} />
+          <Education educations={profileData?.educations} />
+          <Skills />
+        </Grid>
       </Grid>
-    </Grid>
+    </div>
   )
 }
--- src/container/EditProfile/useBasicForm.ts
+++ src/container/EditProfile/useBasicForm.ts
@@ -35,11 +35,14 @@ export default function useBasicForm() {
     control,
     setValue,
     handleSubmit,
+    watch,
   } = useForm<IBasicForm>({ defaultValues, resolver: yupResolver(schema) })
 
   const onSubmit = async (data: IBasicForm) => {
     await putHandler({ endpoint: 'profile', body: { ...data, city: data?.city?.name, country: data?.country?.name } })
   }
+
+  console.log(watch('image'))
 
   return { errors, control, setValue, handleSubmit, onSubmit }
 }
