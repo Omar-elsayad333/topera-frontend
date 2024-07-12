90277f06022f30aa1696e9678d7972a60a7fcc9a
omar elsayad <omarelsayad313@gmail.com>
2024-07-12 18:19:49 +0300

    fix

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -13,36 +13,19 @@ export enum ESocialLogin {
   GitHub,
   LinkedIn,
 }
+export enum ESocialPlatform {
+  Google,
+  GitHub,
+  t,
+  X,
+  LinkedIn,
+  Discord,
+  Microsoft,
+}
 
-<<<<<<< HEAD
 export enum EUserTrackLevel {
   AbsoluteBeginner = 25,
   Beginner = 50,
   Intermediate = 75,
   Advanced = 100,
 }
-export enum ESocialPlatform {
-  Google,
-  GitHub,
-  Facebook,
-=======
-export enum ESocialPlatform {
-  Google,
-  GitHub,
-  t,
->>>>>>> e810ce95de7635fd2b4e259d0f8a9ed6f8d8efe0
-  X,
-  LinkedIn,
-  Discord,
-  Microsoft,
-}
-<<<<<<< HEAD
-=======
-
-export enum EUserTrackLevel {
-  AbsoluteBeginner,
-  Beginner,
-  Intermediate,
-  Advanced,
-}
->>>>>>> e810ce95de7635fd2b4e259d0f8a9ed6f8d8efe0
